04e514dc3db45d87d0e6322360e51b0d245106f0
Dan Hintz <dhintz@live.com>
2018-10-20 04:02:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES 
-  (1, "Mr. Chocolate", 4, "M", "Brown", "Angry", 0),
-  (2, "Rowdy", 2, "M", "Brown", "Rowdy", 1),
-  (3, "Tabitha", 5, "F", "Brown", "Moody", 1),
-  (4, "Sergeant Brown", 3, "Black", "Sarcastic", 0),
-  (5, "Melissa", 10, "Black", "Whimsical", 1),
-  (6, "Grinch", 8, "Brown", "Grinchy", 0),
-  (7, "Wendy", 3, "Brown", "Ornery", 1),
-  (8, NULL, NULL, "Black", "Murderous", 1);
+  ("Mr. Chocolate", 4, "M", "Brown", "Angry", 0),
+  ("Rowdy", 2, "M", "Brown", "Rowdy", 1),
+  ("Tabitha", 5, "F", "Brown", "Moody", 1),
+  ("Sergeant Brown", 3, "Black", "Sarcastic", 0),
+  ("Melissa", 10, "Black", "Whimsical", 1),
+  ("Grinch", 8, "Brown", "Grinchy", 0),
+  ("Wendy", 3, "Brown", "Ornery", 1),
+  (NULL, NULL, "Black", "Murderous", 1);
